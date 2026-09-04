dde9e707688bc419723da0ef616bd65cc2200835
Daniel Morandini <danielmorandini@me.com>
2026-09-04 16:14:05 +0200

    Release moqx 0.8.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MOQX.MixProject do
   def project do
     [
       app: :moqx,
-      version: "0.8.0",
+      version: "0.8.1",
       description: description(),
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
